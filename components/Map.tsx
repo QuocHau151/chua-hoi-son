d0f987391deeaa60e87be8bcf17419098111d494
Quoc Hau <tranlequochau.blc@gmail.com>
2024-07-16 18:39:47 +0700

    fix server

--- components/Map.tsx
+++ components/Map.tsx
@@ -5,7 +5,7 @@ export default function Map() {
   return (
     <section className=" pt-20" id="map">
       <h1 className="text-white text-[30px] text-center px-14 py-5">
-        Sơ đồ chánh điện Chùa Hội Sơn
+        Sơ đồ chánh điện Chùa Hội Sơn {""}
       </h1>
       <Image src="/assets/images/map.jpg" width={1920} height={1080} alt="" />
     </section>
